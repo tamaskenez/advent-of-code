dd61f890ff0d16e0fbbc329c1c7cac200390b445
Tamas Kenez <tamas.kenez@gmail.com>
2020-02-07 09:19:18 +0100

    After 2017/25.

--- 2017/d24.cpp
+++ 2017/d24.cpp
@@ -1,9 +1,103 @@
 #include "common.h"
 
+using PII = pair<int, int>;
+
+void remove(map<int, unordered_map<int, int>>& ps, int a, int b)
+{
+    auto it = ps.find(a);
+    assert(it != ps.end());
+    auto jt = it->second.find(b);
+    assert(jt->second > 0);
+    if (jt->second == 1) {
+        it->second.erase(b);
+    } else {
+        --jt->second;
+    }
+}
+
+int get_best(int last_conn, map<int, unordered_map<int, int>>& ps, int sum_so_far)
+{
+    auto it = ps.find(last_conn);
+    auto& ends_map = it->second;
+    VI endstotry;
+    for (auto& ekv : ends_map) {
+        auto endx = ekv.first;
+        auto count = ekv.second;
+        if (count == 0) {
+            continue;
+        }
+        endstotry.PB(endx);
+    }
+    RunningStat<int> stat;
+    for (auto endx : endstotry) {
+        remove(ps, last_conn, endx);
+        remove(ps, endx, last_conn);
+        auto result = get_best(endx, ps, sum_so_far + last_conn + endx);
+        stat.add(result);
+        ++ps[last_conn][endx];
+        ++ps[endx][last_conn];
+    }
+    if (stat.upper) {
+        return *stat.upper;
+    } else {
+        return sum_so_far;
+    }
+}
+
+pair<int, int> get_best2(int last_conn,
+                         map<int, unordered_map<int, int>>& ps,
+                         pair<int, int> sum_so_far)
+{
+    auto it = ps.find(last_conn);
+    auto& ends_map = it->second;
+    VI endstotry;
+    for (auto& ekv : ends_map) {
+        auto endx = ekv.first;
+        auto count = ekv.second;
+        if (count == 0) {
+            continue;
+        }
+        endstotry.PB(endx);
+    }
+    pair<int, int> best = sum_so_far;
+    for (auto endx : endstotry) {
+        remove(ps, last_conn, endx);
+        remove(ps, endx, last_conn);
+        auto result = get_best2(
+            endx, ps, make_pair(sum_so_far.first + last_conn + endx, sum_so_far.second + 1));
+        if (result.second > best.second ||
+            (result.second == best.second && result.first > best.first)) {
+            best = result;
+        }
+        ++ps[last_conn][endx];
+        ++ps[endx][last_conn];
+    }
+    return best;
+}
+
 int main()
 {
-    ifstream f(CMAKE_CURRENT_SOURCE_DIR "/d3_input.txt");
+    ifstream f(CMAKE_CURRENT_SOURCE_DIR "/d24_input.txt");
     assert(f.good());
     auto lines = read_lines(f);
+    map<int, unordered_map<int, int>> ps;
+
+    for (auto& l : lines) {
+        auto xs = split(l, "/");
+        assert(~xs == 2);
+        int a = stoi(xs[0]);
+        int b = stoi(xs[1]);
+        ++ps[a][b];
+        ++ps[b][a];
+    }
+
+    if (0) {
+        int b = get_best(0, ps, 0);
+        printf("%d\n", b);
+    }
+    if (1) {
+        auto b = get_best2(0, ps, make_pair(0, 0));
+        printf("strength %d length %d\n", b.first, b.second);
+    }
     return 0;
 }
--- 2017/d25.cpp
+++ 2017/d25.cpp
@@ -2,8 +2,94 @@
 
 int main()
 {
-    ifstream f(CMAKE_CURRENT_SOURCE_DIR "/d3_input.txt");
+#if 0
+    ifstream f(CMAKE_CURRENT_SOURCE_DIR "/d25_input.txt");
     assert(f.good());
     auto lines = read_lines(f);
+#endif
+    auto DP = 12368930;
+    map<int, int> tape;
+    char state = 'A';
+    int cursor = 0;
+    FOR (i, 0, < DP) {
+        auto& cv = tape[cursor];
+        switch (state) {
+            case 'A':
+                if (cv == 0) {
+                    cv = 1;
+                    ++cursor;
+                    state = 'B';
+                } else {
+                    cv = 0;
+                    ++cursor;
+                    state = 'C';
+                }
+                break;
+            case 'B':
+                if (cv == 0) {
+                    cv = 0;
+                    --cursor;
+                    state = 'A';
+                } else {
+                    cv = 0;
+                    ++cursor;
+                    state = 'D';
+                }
+                break;
+            case 'C':
+                if (cv == 0) {
+                    cv = 1;
+                    ++cursor;
+                    state = 'D';
+                } else {
+                    cv = 1;
+                    ++cursor;
+                    state = 'A';
+                }
+                break;
+            case 'D':
+                if (cv == 0) {
+                    cv = 1;
+                    --cursor;
+                    state = 'E';
+                } else {
+                    cv = 0;
+                    --cursor;
+                    state = 'D';
+                }
+                break;
+            case 'E':
+                if (cv == 0) {
+                    cv = 1;
+                    ++cursor;
+                    state = 'F';
+                } else {
+                    cv = 1;
+                    --cursor;
+                    state = 'B';
+                }
+                break;
+            case 'F':
+                if (cv == 0) {
+                    cv = 1;
+                    ++cursor;
+                    state = 'A';
+                } else {
+                    cv = 1;
+                    ++cursor;
+                    state = 'E';
+                }
+                break;
+            default:
+                UNREACHABLE;
+        }
+    }
+    int count = 0;
+    for (auto& kv : tape) {
+        if (kv.second) {
+            ++count;
+        }
+    }
+    printf("count: %d\n", count);
     return 0;
 }
