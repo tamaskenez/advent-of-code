8c1badd9f2ea86e800bec823b500088f9eaeb1b6
Tamas Kenez <tamas.kenez@shapr3d.com>
2021-12-21 23:22:55 +0100

    d21

--- 2021/d20.cpp
+++ 2021/d20.cpp
@@ -1,9 +1,141 @@
 #include "common.h"
 
+struct Mi
+{
+    set<AI2> img;
+    bool inverted = false;
+};
+
+// input img    iea
+//  normal     ..    // ok
+//  normal     .#    // ok
+//  normal     #.    // use ~iea, inverse result
+//  normal     ##    // use ~iea, inverse result
+
+// input img    iea
+//  inverse     ..
+//  inverse     .#
+//  inverse     #.
+//  inverse     ##
+
+tuple<AI2, AI2> get_bounds_cc(const set<AI2>& img)
+{
+    AI2 lo = *img.begin();
+    AI2 hi = *img.begin();
+    for (auto rc : img) {
+        FOR (i, 0, < 2) {
+            lo[i] = std::min(lo[i], rc[i]);
+            hi[i] = std::max(hi[i], rc[i]);
+        }
+    }
+    return {lo, hi};
+}
+
+Mi apply_iea(const Mi& mi, const string& iea)
+{
+    assert(~iea == 512);
+    bool raw_outside = false;
+    bool actual_outside = raw_outside ^ mi.inverted;
+    int code_for_outside = actual_outside ? 511 : 0;
+    bool result_actual_outside = iea[code_for_outside] == '#';
+    bool invert_output = result_actual_outside;
+    /*
+if (mi.inverted) {
+    use_inverse_iea = iea[511] == '#';
+} else {
+    use_inverse_iea = iea[0] == '#';
+}
+*/
+
+    set<AI2> result_img;
+    auto [lo, hi] = get_bounds_cc(mi.img);
+    FOR (r, lo[0] - 1, <= hi[0] + 1) {
+        FOR (c, lo[1] - 1, <= hi[1] + 1) {
+            int code = 0;
+            FOR (rr, r - 1, <= r + 1) {
+                FOR (cc, c - 1, <= c + 1) {
+                    auto light = (mi.img.count(AI2{rr, cc}) > 0) ^ mi.inverted;
+                    code = (code << 1) + (light ? 1 : 0);
+                }
+            }
+            assert(is_between_cc(code, 0, 511));
+            auto result_light = (iea[code] == '#') ^ invert_output;
+            if (result_light) {
+                result_img.insert(AI2{r, c});
+            }
+        }
+    }
+
+    /*
+        for (bool raw_input : {false, true}) {
+            bool actual_input = raw_input ^ mi.inverted;
+            bool full_area = iea[actual_input ? 511 : 0] == '#';
+            auto code = (raw_input ^ mi.inverted) ? 511 : 0;
+            bool calculated_actual_output  = iea[code]=='#';
+            bool raw_output = calculated_actual_output ^ use_inverse_iea;
+            assert((raw_output^output_inverted)==full_area);
+            if (!raw_input) {
+                assert(!raw_output);
+            }
+        }
+    */
+    return Mi{move(result_img), invert_output};
+}
+
+void P1(const VS& img, const string& iea)
+{
+    set<AI2> mimg;
+    FOR (r, 0, < ~img) {
+        FOR (c, 0, < ~img[r]) {
+            if (img[r][c] == '#') {
+                mimg.insert(AI2{r, c});
+            }
+        }
+    }
+    auto mi = apply_iea(Mi{mimg, false}, iea);
+    mi = apply_iea(mi, iea);
+    printf("P1: %d\n", ~mi.img);
+}
+
+void P2(const VS& img, const string& iea)
+{
+    set<AI2> mimg;
+    FOR (r, 0, < ~img) {
+        FOR (c, 0, < ~img[r]) {
+            if (img[r][c] == '#') {
+                mimg.insert(AI2{r, c});
+            }
+        }
+    }
+    auto mi = Mi{mimg, false};
+    FOR (step, 0, < 50) {
+        mi = apply_iea(mi, iea);
+    }
+    int count = ~mi.img;
+    if (mi.inverted) {
+        auto [lo, hi] = get_bounds_cc(mi.img);
+        int total = (hi[0] - lo[0] + 1) * (hi[1] - lo[1] + 1);
+        count = total - count;
+    }
+    printf("P2: %d\n", count);
+}
+
 int main()
 {
-    ifstream f(CMAKE_CURRENT_SOURCE_DIR "/d3_input.txt");
+    ifstream f(CMAKE_CURRENT_SOURCE_DIR "/d20_input.txt");
     assert(f.good());
     auto lines = read_lines(f);
+    auto iea = lines[0];
+    VS img;
+    set<int> ws;
+    FOR (i, 2, < ~lines) {
+        img.PB(lines[i]);
+        ws.insert(~lines[i]);
+    }
+    assert(~ws == 1);
+    P1(img, iea);
+    P2(img, iea);
     return 0;
 }
+
+// 16563 high
--- 2021/d21.cpp
+++ 2021/d21.cpp
@@ -1,9 +1,190 @@
 #include "common.h"
 
+struct Game
+{
+    array<int, 2> players;
+    int next_player = 0;
+    int next_dice = 1;
+    array<int, 2> scores = {0, 0};
+    int roll_count = 0;
+
+    int roll()
+    {
+        int r = next_dice++;
+        ++roll_count;
+        if (next_dice == 101) {
+            next_dice = 1;
+        }
+        return r;
+    }
+    void step()
+    {
+        int s = roll() + roll() + roll();
+        auto& p = players[next_player];
+        p = ((p - 1 + s) % 10) + 1;
+        auto& score = scores[next_player];
+        score += p;
+        next_player = 1 - next_player;
+    }
+};
+
+void P1(int pl1, int pl2)
+{
+    Game game;
+    game.players[0] = pl1;
+    game.players[1] = pl2;
+    for (;;) {
+        game.step();
+        optional<int> winner;
+        if (game.scores[0] >= 1000) {
+            winner = 0;
+        } else if (game.scores[1] >= 1000) {
+            winner = 1;
+        }
+        if (winner) {
+            printf("P1: %d\n", game.scores[1 - *winner] * game.roll_count);
+            break;
+        }
+    }
+}
+
+struct G
+{
+    int cell;
+    int steps_left;
+    int score_to_get;
+    bool operator<(const G& y) const
+    {
+        return AI3{cell, steps_left, score_to_get} < AI3{y.cell, y.steps_left, y.score_to_get};
+    }
+};
+
+struct H
+{
+    int cell;
+    int steps_left;
+    int score_to_get_less;
+    bool operator<(const H& y) const
+    {
+        return AI3{cell, steps_left, score_to_get_less} <
+               AI3{y.cell, y.steps_left, y.score_to_get_less};
+    }
+};
+
+vector<pair<int, int>> outcomes = {{1, 3}, {3, 4}, {6, 5}, {7, 6}, {6, 7}, {3, 8}, {1, 9}};
+
+map<G, int64_t> cache;
+map<H, int64_t> cacheb;
+
+int64_t how_many_times_wins(G g)
+{
+    assert(g.steps_left >= 0);
+    if (g.steps_left == 0) {
+        return g.score_to_get <= 0 ? 1 : 0;
+    }
+    assert(g.steps_left > 0);
+    if (g.score_to_get <= 0) {
+        return 0;
+    }
+
+    auto it = cache.find(g);
+    if (it != cache.end()) {
+        return it->second;
+    }
+
+    int64_t result_count = 0;
+    for (auto [times, die] : outcomes) {
+        auto new_cell = ((g.cell - 1 + die) % 10) + 1;
+        result_count +=
+            times * how_many_times_wins(G{new_cell, g.steps_left - 1, g.score_to_get - new_cell});
+    }
+    cache[g] = result_count;
+    return result_count;
+}
+
+int64_t how_many_times_loses(H g)
+{
+    assert(g.steps_left >= 0);
+    if (g.score_to_get_less <= 0) {
+        return 0;
+    }
+    assert(g.score_to_get_less > 0);
+    if (g.steps_left == 0) {
+        return 1;
+    }
+
+    auto it = cacheb.find(g);
+    if (it != cacheb.end()) {
+        return it->second;
+    }
+    assert(g.steps_left > 0);
+    int64_t result_count = 0;
+    for (auto [times, die] : outcomes) {
+        auto new_cell = ((g.cell - 1 + die) % 10) + 1;
+        result_count += times * how_many_times_loses(
+                                    H{new_cell, g.steps_left - 1, g.score_to_get_less - new_cell});
+    }
+    cacheb[g] = result_count;
+    return result_count;
+}
+
+map<AI4, tuple<int64_t, int64_t>> cachec;
+tuple<int64_t, int64_t> number_of_wins_for_both(int pos1, int pos2, int score1, int score2)
+{
+    auto key = AI4{pos1, pos2, score1, score2};
+    auto it = cachec.find(key);
+    if (it != cachec.end()) {
+        return it->second;
+    }
+    tuple<int64_t, int64_t> wins;
+    for (auto [times, die] : outcomes) {
+        auto new_pos1 = (pos1 - 1 + die) % 10 + 1;
+        auto new_score1 = score1 + new_pos1;
+        if (new_score1 >= 21) {
+            get<0>(wins) += times;
+            continue;
+        }
+        auto reverse_wins = number_of_wins_for_both(pos2, new_pos1, score2, new_score1);
+        get<0>(wins) += times * get<1>(reverse_wins);
+        get<1>(wins) += times * get<0>(reverse_wins);
+    }
+    cachec[key] = wins;
+    return wins;
+}
+
 int main()
 {
-    ifstream f(CMAKE_CURRENT_SOURCE_DIR "/d3_input.txt");
-    assert(f.good());
-    auto lines = read_lines(f);
+    // P1(4, 8);
+    P1(2, 1);
+
+    // starting at 1, in 1 step reach 21 how many possible way?
+    auto p1_starts_at = 2;
+    auto p2_starts_at = 1;
+    int64_t p1_wins = 0;
+    int64_t p2_wins = 0;
+    auto winner_score = 21;
+    for (int p1_wins_in_steps = 1; p1_wins_in_steps <= 10; ++p1_wins_in_steps) {
+        auto player1_wins_this_many_times =
+            how_many_times_wins(G{p1_starts_at, p1_wins_in_steps, winner_score});
+        auto player2_loses_this_many_times =
+            how_many_times_loses(H{p2_starts_at, p1_wins_in_steps - 1, winner_score});
+        p1_wins += player1_wins_this_many_times * player2_loses_this_many_times;
+    }
+    for (int p2_wins_in_steps = 1; p2_wins_in_steps <= 10; ++p2_wins_in_steps) {
+        auto player1_loses_this_many_times =
+            how_many_times_loses(H{p1_starts_at, p2_wins_in_steps, winner_score});
+        auto player2_wins_this_many_times =
+            how_many_times_wins(G{p2_starts_at, p2_wins_in_steps, winner_score});
+        p2_wins += player2_wins_this_many_times * player1_loses_this_many_times;
+    }
+    printf("player 1 wins: %lld\nplayer 2 wins: %lld\n", p1_wins, p2_wins);
+    printf("P2: %lld\n", std::max(p1_wins, p2_wins));
+
+    auto [a, b] = number_of_wins_for_both(2, 1, 0, 0);
+    printf("player 1 wins: %lld\nplayer 2 wins: %lld\n", a, b);
+    printf("P2: %lld\n", std::max(a, b));
+
     return 0;
 }
+
+// high 444356092776317
